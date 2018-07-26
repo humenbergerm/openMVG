3862496b4a53ccf842a62a7f8b622d856d888f08
Nicolas Guerin <nicolas.guerin@naverlabs.com>
2018-07-26 17:57:26 +0200

    Add simple build script.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ -z "$SOURCEDIR" ]; then
+    SOURCEDIR="$CURDIR"
+fi
+if [ -z "$BUILD_TYPE" ]; then
+    BUILD_TYPE=Release
+fi
+if [ ! "$BUILD_TYPE" = "Debug" ] && [ ! "$BUILD_TYPE" = "Release" ] && [ ! "$BUILD_TYPE" = "RelWithDebInfo" ]; then
+  echo "Unknown BUILD_TYPE: $BUILD_TYPE, using Release"
+  BUILD_TYPE=Release
+fi
+if [ -z "$BUILD_DIR" ]; then
+  BUILD_DIR=$SOURCEDIR/build/$BUILD_TYPE
+fi
+if [ -z "$INSTALL_DIR" ]; then
+  INSTALL_DIR=$SOURCEDIR/install/$BUILD_TYPE
+fi
+
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+cmake $SOURCEDIR/src/ -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+make -j 10 install
